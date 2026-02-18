9ba4a99d93a36fa3772f38f08b48654ce25d6092
Yusran <yusranmuhammad687@gmail.com>
2026-02-18 07:25:59 +0700

    Update: birtday form restriction

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -179,6 +179,7 @@ export default function RegisterPage() {
                   id="birthday"
                   name="birthday"
                   type="date"
+                  max={new Date().toISOString().split('T')[0]}
                   disabled={loading}
                   className="h-11 text-sm"
                 />
